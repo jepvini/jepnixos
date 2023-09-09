cab384cc8f81f4303051794b2d37c19b440912ed
leo359531 <leos1359531@gmail.com>
2023-09-09 22:16:36 +0200

    sab 9 set 2023, 22:16:36, CEST

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,15 @@ networking = {
   };
 };
 
+# Bluetooth
+hardware.bluetooth.enable = true;
+services.blueman.enable = true;
+hardware.bluetooth.settings = {
+  General = {
+    Enable = "Source,Sink,Media,Socket";
+  };
+};
+
 # Audio
 # rtkit is optional but recommended
 security.rtkit.enable = true;

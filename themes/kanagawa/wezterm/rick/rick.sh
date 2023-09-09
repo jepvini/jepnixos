2ae69b07dbe409e128ee357d2ee187d58dba1bb0
leo359531 <leos1359531@gmail.com>
2023-09-09 18:26:14 +0200

    sab 9 set 2023, 18:26:14, CEST

--- themes/kanagawa/wezterm/rick/rick.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mpvpaper -p -o "input-ipc-server=/tmp/mpv-socket --loop" eDP-1 ~/.config/rick/Rick_Astley_Never_Gonna_Give_You_Up.mp4
--- themes/kanagawa/wezterm/rick/roll.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while true
-do
-    pamixer -i 100
-    pamixer -u
-done

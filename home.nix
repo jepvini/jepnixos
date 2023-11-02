c5b7fa89aae98559e66af40cf46dc652444289d6
leo359531 <leos1359531@gmail.com>
2023-11-02 22:07:11 +0100

    gio 2 nov 2023, 22:07:11, CET

--- home.nix
+++ home.nix
@@ -57,6 +57,32 @@ in {
       userEmail = "leos1359531@gmail.com";
     };
 
+    # NextCloud Client
+    systemd.user = {
+      services.nextcloud-autosync = {
+        Unit = {
+          Description = "Auto sync Nextcloud";
+          After = "network-online.target";
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -n --exclude /home/leo/HomeBK/sync-exclude.lst --path
+       │ /starless /home/leo  https://scatcat.online";
+          TimeoutStopSec = "180";
+          KillMode = "process";
+          KillSignal = "SIGINT";
+        };
+        Install.WantedBy = ["multi-user.target"];
+      };
+      timers.nextcloud-autosync = {
+        Unit.Description = "Automatic sync files with Nextcloud when booted up after 5 minutes then rerun every 60 minutes";
+        Timer.OnBootSec = "5min";
+        Timer.OnUnitActiveSec = "60min";
+        Install.WantedBy = ["multi-user.target" "timers.target"];
+      };
+      startServices = true;
+    };
+
     # Theme selector
     imports = [
       ./dotfiles/dotfiles.nix
